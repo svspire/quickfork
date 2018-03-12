ac10e952110c43135d413e4e616401fbab7b565e
Shannon Spires <ssos@bearlanding.com>
2018-03-11 19:26:22 -0500

    Added stats and populate-database.

--- quickfork.lisp
+++ quickfork.lisp
@@ -77,3 +77,76 @@ cl-user::*installed-systems* for more info."))
 (defmethod ql-dist::install :after ((release ql-dist::release))
   (setf cl-user::*installed-systems* (append cl-user::*installed-systems* (list release))))
 
+(defparameter *projects-directory* "/Users/svspire/Lisp/third-party/quicklisp-projects/projects/"
+  "Quicklisp projects directory")
+
+(defvar *projects-database* nil)
+
+(defun wsp (char)
+  (member char '(#\space #\tab #\return #\linefeed)))
+
+(defun whitespace-partition (string &optional (start 0) (accum nil))
+  "Return a list of substrings from string, where whitespace separates substrings."
+  (cond ((zerop (length string))
+         (nreverse accum))
+        (t
+         (let ((wspointer (position-if #'wsp string :start start)))
+           (cond (wspointer
+                  (cond ((= start wspointer) ; starts with whitespace
+                         (setf start (position-if (complement #'wsp) string :start start))
+                         (if start
+                             (whitespace-partition string start accum)
+                             (nreverse accum)))
+                        (t
+                         (whitespace-partition string (1+ wspointer) (cons (subseq string start wspointer)
+                                                                           accum)))))
+                 (t (nreverse (cons (subseq string start)
+                                    accum))))))))
+
+
+(defun populate-projects (&optional (projects-directory *projects-directory*))
+  "Populate the projects database from the projects directory.
+  Projects database allows us to look up the original repo for a given project."
+  (flet ((get-source (dirpath)
+           "Call on each individual project to return source data"
+           (let ((source-path (merge-pathnames "source.txt"
+                                               dirpath)))
+             (when (probe-file source-path)
+               (let ((raw-string nil)
+                     (data nil))
+                 (with-open-file (s source-path)
+                   (setf raw-string (read-line s nil nil nil)))
+                 (setf data (whitespace-partition raw-string))
+                 (when data
+                   (setf (first data)
+                         (intern (string-upcase (first data))
+                                 #.(find-package "KEYWORD"))))
+                 data)))))
+    
+    (let ((dirpaths (uiop:subdirectories projects-directory)))
+      (mapcar (lambda (dirpath)
+                (cons (car (last (pathname-directory dirpath)))
+                      (get-source dirpath)))
+              dirpaths))))
+
+(defun ensure-projects-database ()
+  (unless *projects-database* (setf *projects-database* (populate-projects))))
+
+
+(defun stats ()
+  "Report number of projects per repo type, sorted most to least."
+  (ensure-projects-database)
+  (let ((tallies (make-hash-table))
+        (alist nil))
+    (flet ((tally (key)
+             (if (gethash key tallies)
+                 (incf (gethash key tallies))
+                 (setf (gethash key tallies) 1))))
+      (mapc (lambda (data)
+              (tally (second data)))
+            *projects-database*)
+      (maphash (lambda (key value)
+                 (setf alist (acons key value alist)))
+               tallies)
+      (sort alist #'> :key #'cdr))))
+
