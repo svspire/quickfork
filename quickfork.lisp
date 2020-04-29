d6cf7929a00aad396b771a644a761b41cd7eb806
Shannon Spires <ssos@bearlanding.com>
2020-04-29 15:27:36 -0600

    Now handles dependency-lookup more robustly i.e. in cases where a dependency is specified in an .asd file as a list like  '(:version "metatilities-base" "0.6.6").

--- quickfork.lisp
+++ quickfork.lisp
@@ -46,7 +46,6 @@ type (e.g. :git, :mercurial, etc.) and their upstream repository location")
                  (t (nreverse (cons (subseq string start)
                                     accum))))))))
 
-
 (defun populate-projects (&optional (projects-directory *projects-directory*))
   "Populate the projects database from the projects directory.
   Projects database allows us to look up the original repo for a given project."
@@ -92,9 +91,131 @@ type (e.g. :git, :mercurial, etc.) and their upstream repository location")
                tallies)
       (sort alist #'> :key #'cdr))))
 
-(defun repo-location (project-name)
+(defmethod repo-location (project-name)
   "Look up the repo for a given project name"
   (ensure-projects-database)
   (when (symbolp project-name)
     (setf project-name (string-downcase (symbol-name project-name))))
   (find project-name *projects-database* :key 'car :test 'string-equal))
+
+(defmethod repo-location ((project asdf::system))
+  (repo-location (asdf::component-name project)))
+
+(defun direct-dependencies (project-name)
+  "Return the direct dependencies of project"
+  (mapcar (lambda (dependency)
+            (asdf::resolve-dependency-spec project-name dependency))
+          (asdf:system-depends-on (asdf:find-system project-name))))
+
+(defun %all-dependencies (project-list accum-table)
+  (when project-list
+    (mapc (lambda (project)
+            (unless (gethash project accum-table)
+              (setf (gethash project accum-table) t)
+              (%all-dependencies (direct-dependencies project) accum-table)))
+          project-list))
+  (loop for k being each hash-key of accum-table collect k))
+
+(defun all-dependencies (project-name)
+  "Return a set of the names of all the dependencies of project."
+  (let ((accum (make-hash-table :test #'equalp)))
+    (%all-dependencies (list project-name) accum)))
+
+; (ql:quickload :rcl) ; to retrieve all dependencies of rcl from the network as needed
+; (all-dependencies :rcl) ; to get a list of all the dependencies of rcl
+; (mapcar 'repo-location (all-dependencies :rcl)) ; to get a list of all the repos you'll need to fork.
+
+(defun ends-with (subseq seq)
+  "Returns t if seq ends with subseq"
+  (let ((lsubseq (length subseq))
+        (lseq (length seq)))
+    (and (> lsubseq 0)
+         (> lseq 0)
+         (>= lseq lsubseq)
+         (let ((pos (search subseq seq :from-end t)))
+           (and pos (= pos (- lseq lsubseq)))))))
+
+(defun make-clone-commands (project-name &optional clone-args)
+  "Prints git clone commands for all dependencies of project that are of type :GIT.
+   clone-args -- if non-nil -- is assumed to be a string such as \"--mirror\" which
+   will be output as arguments to the git clone commands"
+  (flet ((git? (loc)
+           (eq :GIT (second loc)))
+         (ensure-ends-with-git (string)
+           (if (ends-with ".git" string)
+               string
+               (concatenate 'string string ".git"))))
+
+    (let* ((locs (mapcar 'repo-location (all-dependencies project-name)))
+           (gits (remove-if-not #'git? locs))
+           (non-gits (remove-if #'git? locs)))
+
+      ;; Some "non-gits" are actually gits. Find those and move them.
+      (let ((false-non-gits nil))
+        (mapc (lambda (loc)
+                (when (and (ends-with ".git" (third loc))
+                           (search "git" (symbol-name (second loc)) :test #'string-equal))
+                  (push loc false-non-gits)))
+              non-gits)
+        (dolist (loc false-non-gits)
+          (push loc gits)
+          (setf non-gits (remove loc non-gits :test #'equalp))))
+              
+      (dolist (d gits)
+        (format t "~%git clone ~@[~A~] ~S" clone-args (ensure-ends-with-git (third d))))
+      (format t "~%~%Non-git dependencies:")
+      (dolist (d non-gits)
+        (format t "~%~S" d)))))
+    
+#|
+? (qf::make-clone-commands :rcl)
+
+git clone "https://github.com/sionescu/bordeaux-threads.git"
+git clone "https://github.com/melisgl/named-readtables.git"
+git clone "https://gitlab.common-lisp.net/alexandria/alexandria.git"
+git clone "https://github.com/Shinmera/dissect.git"
+git clone "https://github.com/cl-babel/babel.git"
+git clone "https://github.com/trivial-garbage/trivial-garbage.git"
+git clone "https://github.com/Shinmera/array-utils.git"
+git clone "https://github.com/Shinmera/simple-tasks.git"
+git clone "https://github.com/trivial-features/trivial-features.git"
+
+Non-git dependencies:
+("cffi" :HTTPS "https://common-lisp.net/project/cffi/releases/cffi_latest.tar.gz")
+("uiop" :HTTPS "https://common-lisp.net/project/asdf/archives/uiop.tar.gz")
+("rcl" :HTTPS "https://common-lisp.net/project/rcl/rcl.tar.gz")
+NIL
+? 
+
+Note that above is not entirely accurate because quicklisp is not.
+cffi actually has a github repo at https://github.com/cffi/cffi.git
+and uiop is probably best obtained by:
+
+(require "asdf")
+(asdf:load-system "uiop")
+
+Although the master repo for uiop is apparently at
+https://gitlab.common-lisp.net/asdf/asdf
+where it is unfortunately part of the asdf repo.
+
+|#
+#|
+? (qf::make-clone-commands :rcl "--mirror")
+
+git clone --mirror "https://github.com/sionescu/bordeaux-threads.git"
+git clone --mirror "https://github.com/melisgl/named-readtables.git"
+git clone --mirror "https://gitlab.common-lisp.net/alexandria/alexandria.git"
+git clone --mirror "https://github.com/Shinmera/dissect.git"
+git clone --mirror "https://github.com/cl-babel/babel.git"
+git clone --mirror "https://github.com/trivial-garbage/trivial-garbage.git"
+git clone --mirror "https://github.com/Shinmera/array-utils.git"
+git clone --mirror "https://github.com/Shinmera/simple-tasks.git"
+git clone --mirror "https://github.com/trivial-features/trivial-features.git"
+
+Non-git dependencies:
+("cffi" :HTTPS "https://common-lisp.net/project/cffi/releases/cffi_latest.tar.gz")
+("uiop" :HTTPS "https://common-lisp.net/project/asdf/archives/uiop.tar.gz")
+("rcl" :HTTPS "https://common-lisp.net/project/rcl/rcl.tar.gz")
+NIL
+? 
+|#
